46e54b98a827cd5f9c56b49fa1d9233e03daeb3b
kylevdunbar@gmail.com <kylevdunbar@gmail.com>
2022-08-05 09:28:51 -0700

    initial commit

---
+++ src/main/java/com/example/sbcosmos/SpringBootWithCosmosApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbcosmos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithCosmosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithCosmosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sbcosmos/SpringBootWithCosmosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbcosmos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithCosmosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
